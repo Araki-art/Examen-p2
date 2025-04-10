f7f6d677a27be040d2c1559fd20d756ac719150a
EdissonAcosta <edissonjyardith@gmail.com>
2025-04-10 11:36:23 -0500

    Update ExamenP2.py

--- ExamenP2/ExamenP2.py
+++ ExamenP2/ExamenP2.py
@@ -1,1 +1,36 @@
-cual2quier cosa
+# Estructuras principales
+clientes = []
+bienes = {}
+
+# --- Funciones de Cliente ---
+def tomar_datos_cliente():
+    nombre = input("Nombre del cliente: ")
+    identificacion = input("Identificación: ")
+    telefono = input("Teléfono: ")
+    direccion = input("Dirección: ")
+    return (nombre, identificacion, telefono, direccion)
+
+def buscar_cliente_por_id(identificacion):
+    for c in clientes:
+        if c[1] == identificacion:
+            return c
+    return None
+
+def agregar_cliente():
+    cliente = tomar_datos_cliente()
+    if buscar_cliente_por_id(cliente[1]):
+        print(" Ya existe un cliente con esa identificación.")
+    else:
+        clientes.append(cliente)
+        bienes[cliente[1]] = []
+        print(" Cliente agregado con éxito.")
+
+def consultar_cliente():
+    identificacion = input("Ingrese la identificación del cliente: ")
+    cliente = buscar_cliente_por_id(identificacion)
+    if cliente:
+        print(f"Nombre: {cliente[0]}, Teléfono: {cliente[2]}, Dirección: {cliente[3]}")
+    else:
+        print(" Cliente no encontrado.")
+
+def actualizar_
